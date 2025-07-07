2885c9b4af57c88656ce25cc2be7fb942b50ffdd
julian-valencia-neostella <julian.valencia@neostella.com>
2025-07-07 15:14:07 -0500

    Updated dependencies and migrated from nextUi to heroUi

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { NextUIProvider } from "@nextui-org/react";
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router/router.tsx';
+import { HeroUIProvider } from "@heroui/react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router.tsx";
 
-import './index.css';
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NextUIProvider>
+    <HeroUIProvider>
       <main className="dark text-foreground bg-background">
-        <RouterProvider router={ router } />
+        <RouterProvider router={router} />
       </main>
-    </NextUIProvider>
-  </React.StrictMode>,
-)
+    </HeroUIProvider>
+  </React.StrictMode>
+);
--- src/products/pages/NewProduct.tsx
+++ src/products/pages/NewProduct.tsx
@@ -1,49 +1,48 @@
-import { Button, Image, Input, Textarea } from "@nextui-org/react";
+import {
+  Button,
+  Input,
+  Textarea,
+  Image,
+  Select,
+  SelectItem,
+} from "@heroui/react";
 
 export const NewProduct = () => {
-
-  
-
   return (
     <div className="w-full flex-col">
       <h1 className="text-2xl font-bold">Nuevo producto</h1>
 
-      <form className="w-full">
-
-        <div className="flex justify-around items-center">
-          
-          <div className="flex-col w-[500px]">
-
+      <div className="flex justify-around items-center">
+        <div className="flex-col w-[500px]">
+          <form className="w-full">
             <Input className="mt-2" type="text" label="Titulo del producto" />
             <Input className="mt-2" type="number" label="Precio del producto" />
             <Input className="mt-2" type="url" label="Url del producto" />
             <Textarea className="mt-2" label="Descripcion del producto" />
-            <select className="rounded-md p-3 mt-2 bg-gray-800 w-full">
-              <option value="men's clothing">Men's clothing</option>
-              <option value="women's clothing">Women's clothing</option>
-              <option value="jewelery">Jewelery</option>
-              <option value="electronics">Electronics</option>
-            </select>
+            <Select className="mt-2" label="Categoria del producto">
+              <SelectItem key="men's clothing">Men's clothing</SelectItem>
+              <SelectItem key="women's clothing">Women's clothing</SelectItem>
+              <SelectItem key="jewelery">Jewelery</SelectItem>
+              <SelectItem key="electronics">Electronics</SelectItem>
+            </Select>
 
             <br />
-            <Button className="mt-2" color="primary">Crear</Button>
-          </div>
-
-          <div className="bg-white rounded-2xl p-10 flex items-center" style={{
-            width: '500px',
-            height: '600px',
-          }}>
-
-            <Image
-              src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
-            />
-          </div>
-          
+            <Button className="mt-2" color="primary">
+              Crear
+            </Button>
+          </form>
         </div>
 
-
-      </form>
-
+        <div
+          className="bg-white rounded-2xl p-10 flex items-center"
+          style={{
+            width: "500px",
+            height: "600px",
+          }}
+        >
+          <Image src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
